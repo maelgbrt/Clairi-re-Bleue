5150b8cc98d8654c33f9a9a28ff39ccf352f063e
GekoPlay <97613551+GekoPlay@users.noreply.github.com>
2026-05-07 12:40:11 +0200

    maj avec envoit du mesasge form contact

--- html/js/app_contact.js
+++ html/js/app_contact.js
@@ -12,9 +12,18 @@ const message = ref({
   "text" : null
 })
 
+const notif = ref();
+
 const sendMessage = () => {
   console.log(message.value);
-  
+  axios.get("../../php/admin/message").then(response =>{
+    if (response.data.status == "Success"){
+      notif.value = "Message envoyé avec Succes";
+    }else{
+      notif.value = "Erreur lors de l'envoie du Message";
+    }
+  })
+
 }
 
 
